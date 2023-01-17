abd31c3e18bf50a9bff9a68223805f7a73f38bd1
Daniel Gonzalez <daniel.gonzalez.02@gmail.com>
2023-01-17 14:33:20 +0100

    add jsdoc to search by tag function

--- scripts/utils/searchByTags.js
+++ scripts/utils/searchByTags.js
@@ -14,67 +14,13 @@ export const searchByTags = (userTag, tagsArray) => {
 };
 
 /**
- * filter a list of recipes according to a list of ingredients
- * @param {object} ingredientsList array with all the selected ingredients for the research
+ *
+ * @param {object} ingredientsList array with all ingredients selected by the user
+ * @param {object} appliancesList array with all appliances selected by the user
+//  * @param {object} ustensilsList array with all ustensils selected by the user
  * @param {object} recipesArray list of recipes to filter
+ * @returns
  */
-export const searchByIngredients = async (ingredientsList, recipesArray) => {
-  if (ingredientsList.length > 0) {
-    ingredientsList.forEach((ingredientTag) => {
-      recipesArray = recipesArray.filter((recipe) => {
-        return recipe.ingredients.some((ingredient) =>
-          ingredient.ingredient
-            .toLowerCase()
-            .includes(ingredientTag.name.toLowerCase())
-        );
-      }); //end filter
-    });
-    return recipesArray;
-  } else {
-    return recipesArray;
-  }
-};
-
-/**
- * filter a list of recipes according to a list of appliances
- * @param {object} appliancesList array with all the selected appliances for the research
- * @param {object} recipesArray list of recipes to filter
- */
-export const searchByAppliances = async (appliancesList, recipesArray) => {
-  if (appliancesList.length > 0) {
-    appliancesList.forEach((applianceTag) => {
-      recipesArray = recipesArray.filter((recipe) => {
-        return recipe.appliance
-          .toLowerCase()
-          .includes(applianceTag.name.toLowerCase());
-      }); //end filter
-    });
-    return recipesArray;
-  } else {
-    return recipesArray;
-  }
-};
-
-/**
- * filter a list of recipes according to a list of ustensils
- * @param {object} ustensilsList array with all the selected ustensils for the research
- * @param {object} recipesArray list of recipes to filter
- */
-export const searchByUstensils = async (ustensilsList, recipesArray) => {
-  if (ustensilsList.length > 0) {
-    ustensilsList.forEach((ustensilTag) => {
-      recipesArray = recipesArray.filter((recipe) => {
-        return recipe.ustensils.some((ustensil) =>
-          ustensil.toLowerCase().includes(ustensilTag.name.toLowerCase())
-        );
-      }); //end filter
-    });
-    return recipesArray;
-  } else {
-    return recipesArray;
-  }
-};
-
 export const advancedSearchByTags = async (
   ingredientsList,
   appliancesList,
@@ -150,3 +96,65 @@ export const advancedSearchByTags = async (
   }
   return recipesArray;
 };
+
+// /**
+//  * filter a list of recipes according to a list of ingredients
+//  * @param {object} ingredientsList array with all the selected ingredients for the research
+//  * @param {object} recipesArray list of recipes to filter
+//  */
+// export const searchByIngredients = async (ingredientsList, recipesArray) => {
+//   if (ingredientsList.length > 0) {
+//     ingredientsList.forEach((ingredientTag) => {
+//       recipesArray = recipesArray.filter((recipe) => {
+//         return recipe.ingredients.some((ingredient) =>
+//           ingredient.ingredient
+//             .toLowerCase()
+//             .includes(ingredientTag.name.toLowerCase())
+//         );
+//       }); //end filter
+//     });
+//     return recipesArray;
+//   } else {
+//     return recipesArray;
+//   }
+// };
+
+// /**
+//  * filter a list of recipes according to a list of appliances
+//  * @param {object} appliancesList array with all the selected appliances for the research
+//  * @param {object} recipesArray list of recipes to filter
+//  */
+// export const searchByAppliances = async (appliancesList, recipesArray) => {
+//   if (appliancesList.length > 0) {
+//     appliancesList.forEach((applianceTag) => {
+//       recipesArray = recipesArray.filter((recipe) => {
+//         return recipe.appliance
+//           .toLowerCase()
+//           .includes(applianceTag.name.toLowerCase());
+//       }); //end filter
+//     });
+//     return recipesArray;
+//   } else {
+//     return recipesArray;
+//   }
+// };
+
+// /**
+//  * filter a list of recipes according to a list of ustensils
+//  * @param {object} ustensilsList array with all the selected ustensils for the research
+//  * @param {object} recipesArray list of recipes to filter
+//  */
+// export const searchByUstensils = async (ustensilsList, recipesArray) => {
+//   if (ustensilsList.length > 0) {
+//     ustensilsList.forEach((ustensilTag) => {
+//       recipesArray = recipesArray.filter((recipe) => {
+//         return recipe.ustensils.some((ustensil) =>
+//           ustensil.toLowerCase().includes(ustensilTag.name.toLowerCase())
+//         );
+//       }); //end filter
+//     });
+//     return recipesArray;
+//   } else {
+//     return recipesArray;
+//   }
+// };
